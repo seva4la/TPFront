34cc45343f386d5a97fc1c6de1a1378c4ac0cbea
Ольга <you@example.com>
2024-05-05 19:19:13 +0700

    adding thems

--- src/components/MyModal/MyModal.jsx
+++ src/components/MyModal/MyModal.jsx
@@ -15,3 +15,52 @@ export const MyModal = ({ children, visible, setVisisble }) => {
     </div>
   );
 };
+
+
+import { Check } from 'tabler-icons-react';
+import React from "react";
+import cl from "./MyModal.module.css";
+export const MyModal1 = ({ children, visible, setVisisble }) => {
+  return (
+    <div className={`${cl.Modal} ${visible && cl.active}`}>
+      <div className={cl.ModalContent} onClick={(e) => e.stopPropagation()}>
+        <button onClick={() => setVisisble(false)}>
+          <Check />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+import { Edit } from 'tabler-icons-react';
+import React from "react";
+import cl from "./MyModal.module.css";
+export const MyModal2 = ({ children, visible, setVisisble }) => {
+  return (
+    <div className={`${cl.Modal} ${visible && cl.active}`}>
+      <div className={cl.ModalContent} onClick={(e) => e.stopPropagation()}>
+        <button onClick={() => setVisisble(false)}>
+          <Edit />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+import { NotesOff } from 'tabler-icons-react';
+import React from "react";
+import cl from "./MyModal.module.css";
+export const MyModal3 = ({ children, visible, setVisisble }) => {
+  return (
+    <div className={`${cl.Modal} ${visible && cl.active}`}>
+      <div className={cl.ModalContent} onClick={(e) => e.stopPropagation()}>
+        <button onClick={() => setVisisble(false)}>
+          <NotesOff />
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
