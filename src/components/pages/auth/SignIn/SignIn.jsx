3fc93c1466b2acc218046a8e9975f6fcfbc547cd
Subbotina Olesya <elizabethfox434@gmail.com>
2024-05-19 01:10:42 +0700

    создала страницу авторизации

--- src/components/pages/auth/SignIn/SignIn.jsx
+++ src/components/pages/auth/SignIn/SignIn.jsx
@@ -1,3 +1,60 @@
+import React, { useState } from "react";
+import "./SignIn.css";
 export const SignIn = () => {
-  return <div></div>;
+  const [form, setForm] = useState({
+    firstName: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission
+    console.log("Form submitted:", form);
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="register-container">
+        <div className="register-form">
+          <h1>Авторизироваться</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="firstName">Имя</label>
+              <input
+                type="text"
+                id="firstName"
+                name="firstName"
+                value={form.firstName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Пароль</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={form.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <button type="submit" className="register-button">
+              Войти
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
